ee439dac4640768681afe6a40b76a268e67d2a77
backfromexile <backfromexile@web.de>
2025-02-25 23:12:29 +0100

    update build

--- assets/subset-worker.chunk-CPStkNRO.js
+++
@@ -1,1 +0,0 @@
-import{Commands as e,subsetToBinary as t}from"./subset-shared.chunk-C7bxAML0.js";import"./index-C4DKxLeQ.js";var m=import.meta.url?new URL(import.meta.url):void 0;typeof window>"u"&&typeof self<"u"&&(self.onmessage=async a=>{switch(a.data.command){case e.Subset:let s=await t(a.data.arrayBuffer,a.data.codePoints);self.postMessage(s,{transfer:[s]});break}});export{m as WorkerUrl};
